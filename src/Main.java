48df9ebb725794fdeace2cfe02ecb56251b7ef70
evelinabublyk <evelinabublykedu@gmail.com>
2025-09-20 15:11:48 +0200

    first commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner;
+        while(true){
+            System.out.println("Enter a number from 1 to 4");
+
+
+        }
+
+
+    }
+}
